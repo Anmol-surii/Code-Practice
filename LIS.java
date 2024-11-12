09358a8d6440b54e09751b38bdaea5288d4f0ef2
Anmol-surii <anmolsuri321@gmail.com>
2024-11-13 02:06:27 +0530

    LIS with Binary Search

--- LIS.java
+++ LIS.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class LIS {
     public int lengthOfLIS(int[] nums) {
@@ -16,5 +18,44 @@ public class LIS {
             }
         }
         return lis;
+    }
+
+
+
+    // This is the binary search n*logn solution
+    public int lengthOfLISBinarySearch(int[] nums) {
+        int n = nums.length;
+        List<Integer> temp = new ArrayList<>();
+        temp.add(nums[0]);
+        
+        for(int i = 1; i < n; i++) {
+            // find the index position in the temp array
+            int index = findIndex(temp, nums[i]);
+
+            // if at last it needs to be added
+            if(index == -1) {
+                temp.add(nums[i]);
+            } else {
+                // set the value at index
+                temp.set(index, nums[i]);
+            }
+        }
+        return temp.size();
+    }
+
+    private int findIndex(List<Integer> temp, int x) {
+        int l = 0;
+        int r = temp.size() - 1;
+
+        while(l <= r) {
+            int mid = l + (r - l)/2;
+            if(x <= temp.get(mid)) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return l == temp.size() ? -1 : l;
     }
 }
